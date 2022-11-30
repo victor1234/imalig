f1a49cbf266e963dedca4bf2c72c3d51125570b6
Kataev Victor <kataev.victor.1234@gmail.com>
2022-11-30 10:21:57 +0000

    Add marker detection

--- tests/imalig_tests.cpp
+++ tests/imalig_tests.cpp
@@ -1,5 +1,7 @@
 #include  <catch2/catch_test_macros.hpp>
+#include <opencv2/aruco/dictionary.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
 
 #include <imalig/imalig.hpp>
 
@@ -8,9 +10,20 @@ TEST_CASE("Main")
 {
   cv::Mat barcode = cv::imread("fixtures/barcode.png");
   cv::Mat image = cv::imread("fixtures/image.jpg");
+
+  /* Create aruco dictionary */
+  cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
   
+  auto parameters = cv::aruco::DetectorParameters::create();
+  std::vector<int> markersId;
+  std::vector<std::vector<cv::Point2f>> markersCorners, rejectedCandidates;
+  cv::aruco::detectMarkers(image, dictionary, markersCorners, markersId, parameters, rejectedCandidates);
+
   cv::imshow("barcode", barcode);
   cv::imshow("image", image);
+  cv::Mat outImage = image.clone();
+  cv::aruco::drawDetectedMarkers(outImage, markersCorners, markersId);
+  imshow("result", outImage);
   cv::waitKey(0);
 
 
