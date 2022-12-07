b4bba0816f7991ea71af1f91161ece630d30f4b7
Kataev Victor <kataev.victor.1234@gmail.com>
2022-12-07 16:57:02 +0000

    First run

--- examples/imalig_example.cpp
+++ examples/imalig_example.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
+#include <opencv2/aruco.hpp>
 #include <opencv2/aruco/dictionary.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/aruco.hpp>
 
 #include <imalig/imalig.hpp>
 
-
 int main(int argc, char *argv[])
 {
-  if (argc < 3) {
-    std::cout << "Usage: " << argv[0] << " <barcode> <image>" << std::endl;
-    return 1;
-  }
+	if (argc < 3) {
+		std::cout << "Usage: " << argv[0] << " <barcode> <image>" << std::endl;
+		return 1;
+	}
 
-  /*Load images in grayscale */
-  cv::Mat barcode = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
-  cv::Mat image = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+	/*Load images in grayscale */
+	cv::Mat barcode = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+	cv::Mat image = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
 
-  /* Create aruco dictionary */
-  cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-  
-  /* Detect aruco markers */
-  auto parameters = cv::aruco::DetectorParameters::create();
-  std::vector<int> markersId;
-  std::vector<std::vector<cv::Point2f>> markersCorners, rejectedCandidates;
-  cv::aruco::detectMarkers(image, dictionary, markersCorners, markersId, parameters, rejectedCandidates);
+	/* Create aruco dictionary */
+	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
 
-  /* Show result */
-  cv::imshow("barcode", barcode);
-  cv::imshow("image", image);
-  cv::Mat outImage = image.clone();
-  cv::aruco::drawDetectedMarkers(outImage, markersCorners, markersId);
-  imshow("result", outImage);
-  cv::waitKey(0);
+	/* Detect aruco markers */
+	auto parameters = cv::aruco::DetectorParameters::create();
+	std::vector<int> markersId;
+	std::vector<std::vector<cv::Point2f>> markersCorners, rejectedCandidates;
+	cv::aruco::detectMarkers(image, dictionary, markersCorners, markersId, parameters, rejectedCandidates);
 
+	/* Show result */
+	//cv::imshow("barcode", barcode);
+	//cv::imshow("image", image);
+	//cv::Mat outImage = image.clone();
+	//cv::aruco::drawDetectedMarkers(outImage, markersCorners, markersId);
+	//imshow("result", outImage);
+	//cv::waitKey(0);
 
-  /* Run imalig */
+	/* Run imalig */
 	auto corners = imalig::imalig(barcode, image, markersId[0], markersCorners[0]);
+	std::cout << "corners = " << corners << std::endl;
 
-  cv::Mat outImage2 = image.clone();
-  cv::aruco::drawDetectedMarkers(outImage2, corners, markersId);
+	//cv::Mat outImage2 = image.clone();
+	//cv::aruco::drawDetectedMarkers(outImage2, corners, markersId);
 
-  imshow("result", outImage2);
-  cv::waitKey(0);
+	//imshow("result", outImage2);
+	//cv::waitKey(0);
 
-  return 0;
+	return 0;
 }
--- sources/imalig.cpp
+++ sources/imalig.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 
 #include <memory>
-#include <opencv2/core/types.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <Eigen/Dense>
 #include <ceres/ceres.h>
 #include <ceres/cubic_interpolation.h>
-#include <Eigen/Dense>
-
 
 namespace imalig {
 
@@ -18,10 +17,10 @@ struct CostFunctor {
 	using Interpolator = ceres::BiCubicInterpolator<Grid2D>;
 
 	CostFunctor(const cv::Mat barcode, const cv::Mat image)
-		: barcode(barcode), barcodePoints(3, barcode.rows * barcode.cols), image(image)
+		: barcodePoints(3, barcode.rows * barcode.cols)
 	{
-		barcode.convertTo(barcode, CV_64FC1);
-		image.convertTo(image, CV_64FC1);
+		barcode.convertTo(barcode64, CV_64F);
+		image.convertTo(image64, CV_64F);
 
 		/* Create barcode pixel coordinate matrix */
 		int j = 0;
@@ -34,17 +33,19 @@ struct CostFunctor {
 			}
 		}
 
+		std::cout << "=====" << std::endl;
 		/* Create image interpolator */
-		ceres::Grid2D<double, 1> imageGrid(reinterpret_cast<double*>(barcode.data), 0, barcode.rows, 0, barcode.cols);
+		ceres::Grid2D<double, 1> imageGrid(reinterpret_cast<double *>(image64.data), 0, image64.rows, 0, image64.cols);
 		interpolator = std::make_unique<Interpolator>(imageGrid);
 	}
 
-	template <typename T>
-	bool operator()(const T* const h, T* residual) const {
-		Eigen::Matrix<T, 3, 3> H{h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7], h[8]};
+	template <typename T> bool operator()(const T *const h, T *residual) const
+	{
+		Eigen::Matrix<T, 3, 3> H;
+		H << h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7], h[8];
 
 		Eigen::MatrixX<T> imagePoints = H * barcodePoints;
-		
+
 		for (int j = 0; j < barcodePoints.cols(); j++) {
 			T x = imagePoints(0, j) / imagePoints(2, j);
 			T y = imagePoints(1, j) / imagePoints(2, j);
@@ -52,15 +53,15 @@ struct CostFunctor {
 			T value;
 			interpolator->Evaluate(y, x, &value);
 
-			residual[j] = value - barcode.data[sizeof(double) * j];
+			residual[j] = value - T(barcode64.data[sizeof(double) * j]);
 		}
 
 		return true;
 	}
 
-private:
-	cv::Mat barcode;
-	cv::Mat image;
+  private:
+	cv::Mat barcode64;
+	cv::Mat image64;
 	Eigen::MatrixXd barcodePoints;
 	std::unique_ptr<Interpolator> interpolator;
 };
@@ -74,7 +75,7 @@ std::vector<cv::Point2f> imalig(const cv::Mat barcode, cv::Mat image, const int 
 	cv::Mat markerCorners0;
 	cv::Mat(markerCorners2i).convertTo(markerCorners0, CV_32F);
 
-	cv::Mat H = cv::getPerspectiveTransform(markerCorners0, markerCorners);
+	cv::Mat H = cv::getPerspectiveTransform(markerCorners0, 10 + cv::Mat(markerCorners));
 	std::cout << "H = " << H << std::endl;
 
 	/* Create ceres problem */
@@ -94,7 +95,6 @@ std::vector<cv::Point2f> imalig(const cv::Mat barcode, cv::Mat image, const int 
 	ceres::Solve(options, &problem, &summary);
 
 	std::cout << summary.FullReport() << std::endl;
-
 
 	return {};
 }
