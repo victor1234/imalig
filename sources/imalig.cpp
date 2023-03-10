8279f86ca55c9cc186f995c88c775a661fe69f1d
Victor Kataev <kataev.victor.1234@gmail.com>
2023-03-10 17:06:08 +0400

    Add debug draw

--- sources/imalig.cpp
+++ sources/imalig.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/core/types.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include <Eigen/Dense>
@@ -15,6 +16,59 @@
 #include <spdlog/spdlog.h>
 
 namespace imalig {
+
+std::vector<cv::Point2f> cornersFromH(const std::vector<cv::Point2f> corners0, const cv::Mat H)
+{
+	std::vector<cv::Point2f> corners;
+	for (const auto &cp : corners0) {
+		// std::cout << cp << std::endl;
+		cv::Vec3d p(cp.x, cp.y, 1);
+		cv::Vec3d p2 = cv::Matx33d(H) * p;
+		// std::cout << p2 << std::endl;
+		corners.emplace_back(cv::Point2d(p2[0] / p2[2], p2[1] / p2[2]));
+	}
+	return corners;
+}
+
+class DrawCornersCallback : public ceres::IterationCallback {
+  public:
+	DrawCornersCallback(const cv::Mat &image, const std::vector<cv::Point2f> &corners0, const cv::Mat &H)
+		: image_(image), corners0_(corners0), H_(H)
+	{
+	}
+
+	ceres::CallbackReturnType operator()(const ceres::IterationSummary &summary) override
+	{
+		cv::Mat image = image_.clone();
+		cv::cvtColor(image, image, cv::COLOR_GRAY2BGR);
+
+		const auto corners_ = cornersFromH(corners0_, H_);
+
+		cv::line(image, corners_[0], corners_[1], cv::Scalar(0, 0, 255), 1);
+		cv::line(image, corners_[1], corners_[2], cv::Scalar(0, 0, 255), 1);
+		cv::line(image, corners_[2], corners_[3], cv::Scalar(0, 0, 255), 1);
+		cv::line(image, corners_[3], corners_[0], cv::Scalar(0, 0, 255), 1);
+
+		// draw iteration
+		cv::putText(image, std::to_string(summary.iteration), cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5,
+					cv::Scalar(0, 0, 255), 1);
+		// draw cost
+		cv::putText(image, std::to_string(summary.cost), cv::Point(10, 40), cv::FONT_HERSHEY_SIMPLEX, 0.5,
+					cv::Scalar(0, 0, 255), 1);
+
+		// print H
+		// std::cout << "H: " << summary.user_state[0] << std::endl;
+
+		cv::imshow("corners", image);
+		cv::waitKey(100);
+		return ceres::SOLVER_CONTINUE;
+	}
+
+  private:
+	cv::Mat image_;
+	std::vector<cv::Point2f> corners0_;
+	cv::Mat H_;
+};
 
 std::vector<cv::Point2f> Imalig::process(const cv::Mat barcode, cv::Mat image, const int markerId,
 										 const std::vector<cv::Point2f> markerCorners)
@@ -46,20 +100,15 @@ std::vector<cv::Point2f> Imalig::process(const cv::Mat barcode, cv::Mat image, c
 	options.minimizer_progress_to_stdout = false;
 	options.max_num_iterations = 1000;
 	options.logging_type = ceres::SILENT;
+	options.update_state_every_iteration = true;
+	options.callbacks = {new DrawCornersCallback(image, markerCorners0, H)};
 	ceres::Solver::Summary summary;
 	ceres::Solve(options, &problem, &summary);
 
 	// spdlog::info(summary.FullReport());
 
 	/* Compute new marker corners */
-	std::vector<cv::Point2f> preciseMarkerCorners;
-	for (const auto &cp : markerCorners2i) {
-		// std::cout << cp << std::endl;
-		cv::Vec3d p(cp.x, cp.y, 1);
-		cv::Vec3d p2 = cv::Matx33d(H) * p;
-		// std::cout << p2 << std::endl;
-		preciseMarkerCorners.emplace_back(cv::Point2d(p2[0] / p2[2], p2[1] / p2[2]));
-	}
+	auto preciseMarkerCorners = cornersFromH(markerCorners0, H);
 
 	// spdlog::info("d: {:.2f} {:.2F}", d[0], d[1]);
 
