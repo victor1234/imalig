228dc587599532ac95d2149f6f49b9b4405b676a
Victor Kataev <kataev.victor.1234@gmail.com>
2023-04-14 14:52:52 +0400

    Add write undetected image

--- examples/imalig_example.cpp
+++ examples/imalig_example.cpp
@@ -21,12 +21,29 @@ cv::Vec3d getCameraPosion(cv::Vec3d rvec, cv::Vec3d tvec)
 	cv::Vec3d C = -R.t() * tvec;
 	return C;
 }
+void saveResult(const std::string &originalFilepath, const std::string &directory, const cv::Mat &outImage)
+{
+	std::filesystem::path p(originalFilepath);
+	std::filesystem::path filename = p.filename();
+
+	p.remove_filename();
+	p.append("result");
+
+	std::filesystem::create_directories(p);
+
+	p /= filename;
+
+	spdlog::info("Save result in {}", p.string());
+
+	cv::imwrite(p.string(), outImage);
+}
 
 int main(int argc, char *argv[])
 {
 	int marker_id;
 	std::string image_filename;
 	float resizeRatio;
+	std::string outputDirectory;
 
 	/* Configure argumentum */
 	auto parser = argument_parser{};
@@ -35,6 +52,10 @@ int main(int argc, char *argv[])
 	params.add_parameter(marker_id, "id").help("Marker ID");
 	params.add_parameter(image_filename, "image").help("Image filepath");
 	params.add_parameter(resizeRatio, "--resize", "-r").nargs(1).help("Resize ratio").default_value(0.5);
+	params.add_parameter(outputDirectory, "--output-subdirectory", "-o")
+		.nargs(1)
+		.help("Output subdirectory")
+		.default_value("result");
 
 	if (!parser.parse_args(argc, argv, 1)) {
 		return 1;
@@ -60,6 +81,7 @@ int main(int argc, char *argv[])
 	/* Check if markers is detected */
 	if (markersId.empty()) {
 		spdlog::error("No markers detected");
+		saveResult(image_filename, outputDirectory, image);
 		return 1;
 	}
 
@@ -90,7 +112,7 @@ int main(int argc, char *argv[])
 	cv::Mat distCoeffs = cv::Mat::zeros(4, 1, CV_64F);
 	std::vector<cv::Vec3d> rvec, tvec;
 	cv::aruco::estimatePoseSingleMarkers(markersCorners, 0.25956, cameraMatrix, distCoeffs, rvec, tvec);
-	cv::aruco::drawAxis(outImage, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
+	cv::aruco::drawAxis(outImage, cameraMatrix, distCoeffs, rvec, tvec, 0.5);
 
 	/* Print rvec and tvec */
 	// spdlog::info("rvec = {}", rvec[0]);
@@ -111,14 +133,7 @@ int main(int argc, char *argv[])
 				cv::Point(10, outImage.rows - 30), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0, 200, 0), 3);
 
 	/* Save result in diffirent directory */
-	std::filesystem::path p(image_filename);
-	std::filesystem::path filename = p.filename();
-	p.remove_filename();
-	p.append("result");
-	std::filesystem::create_directories(p);
-	p /= filename;
-	spdlog::info("Save result in {}", p.string());
-	cv::imwrite(p.string(), outImage);
+	saveResult(image_filename, outputDirectory, outImage);
 
 	/* Show result */
 	if (resizeRatio > 0) {
